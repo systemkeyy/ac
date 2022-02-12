68557251e576b42af174bdf043269823c18c94f8
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-12 09:58:56 +0100

    Create web.sh

---
+++ web.sh
@@ -0,0 +1,132 @@
+clear &&
+echo ""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' UNTZ'\033[0m' 
+sleep 0.3 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' UNTZUNTZ '\033[0m'
+sleep 0.3 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' UNTZUNTZUNTZ '\033[0m'
+sleep 0.3 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' UNTZUNTZUNTZUNTZ '\033[0m'
+sleep 0.3 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' UNTZUNTZUNTZUNTZUNTZ '\033[0m'   
+sleep 4  &&
+sudo apt update && sudo apt upgrade -y && sudo apt install mysql-server && chmod +x coin.pl &&
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+clear &&
+echo ""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' PHPSERVER SETUP'\033[0m' 
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' WE WILL MAKE SURE THAT ALL SOFTWARE NEEDED IS CURRENTLY INSTALLED'\033[0m' 
+sleep 1  &&
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m' SCRIPT BY SYSTEMKEYY'\033[0m' 
+sleep 3
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'Preparing to Install & Secure'.';
+echo -ne '>                       [5%]\r'
+sleep 0.1
+echo -ne '>>                      [10%]\r'
+sleep 0.2
+echo -ne '>>>>                    [20%]\r'
+sleep 0.1
+echo -ne '>>>>>                   [25%]\r'
+sleep 0.1
+echo -ne '>>>>>>                  [30%]\r'
+sleep 0.1
+echo -ne '>>>>>>>                 [35%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>                [40%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>               [45%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>              [50%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>             [55%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>            [60%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>           [65%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>          [70%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>         [75%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>        [80%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>       [85%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>      [90%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>     [95%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>>>   [100%]\r.'
+sleep 2 &&
+read -p "We need to check for missing files and clone into a reseptory, do you comply? <yes/no> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+ sudo apt install nginx tor gcc make build-essential autoconf libtool libsodium-dev php7.4 php7.4-mysql php7.4-curl php7.4-json php7.4-cgi php7.4-xsl php7.4-fpm -yy && bash coin.pl;
+ else
+    echo 'exit'
+fi
